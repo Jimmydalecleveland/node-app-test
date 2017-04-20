d7fb1635325f4c0241d4282931d211f6be67e28c
jimmy <jimmydalecleveland@gmail.com>
2017-04-20 16:30:35 -0600

    initial commit

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+    {
+      name      : 'API',
+      script    : 'app.js',
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'root',
+      host : '138.68.227.13',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '~/node-app',
+      'post-deploy' : 'nvm install && npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
