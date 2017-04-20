72026a11ba719f318c6230aa352e93f4d4bdb315
jimmy <jimmydalecleveland@gmail.com>
2017-04-20 16:40:47 -0600

    ecosystem: add direct pm2 directory

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,7 +24,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'https://github.com/Jimmydalecleveland/node-app-test.git',
       path : '~/node-app',
-      'post-deploy' : 'nvm install && npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'nvm install && npm install && /root/.nvm/versions/node/v6.10.2/bin/pm2 reload ecosystem.config.js --env production'
     }
   }
 };
